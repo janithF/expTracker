72fd29f5b5aba76de9a0131ef88e55ba11dc2fee
Janith Fernando <janithnfernando@gmail.com>
2024-12-14 12:35:45 +0530

    App component layout update

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
   const [selectedBudget, setSelectedBudget] = useState<Budget>(budgetList[0]);
 
   return (
-    <div id="expense-tracker" className="flex h-full border bg-background border-gray-200 border-solid rounded-[25px]">
+    <div id="expense-tracker" className="flex h-full border bg-background border-gray-200 border-solid rounded-[25px] max-w-screen-2xl mx-auto">
       <div id="side-panel-container" className="w-[300px]  rounded-l-[25px] py-[5px] px-[12px]">
         <SidePanel budgetList={budgetList} selectedBudget={selectedBudget} onSelectBudget={(budget) => setSelectedBudget(budget)} />
       </div>
-      <div id="main-container" className="flex-1 rounded-[25px] py-[5px] px-[12px] bg-white">
+      <div id="main-container" className="flex-1 rounded-[25px] py-[5px] px-[20px] bg-white">
         <MainContent budget={selectedBudget}/>
       </div>
     </div>
